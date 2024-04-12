3a661b13a0b98f0293b2e3ded9b139424a175f95
Abeypala P.S.D. <abeypalapsd.21@itfac.mrt.ac.lk>
2024-04-12 17:58:28 +0530

    removing conflicts

--- src/pages/Manage_Equipment.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Manage_Equipment(){
-    return(
-        <div>
-            <h1>This is the Manage Equipment</h1>
-        </div>
-    )
-}
--- src/pages/Video_Feed.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Video_Feed(){
-    return(
-        <div>
-            <h1>This is the Video Feed</h1>
-        </div>
-    )
-}
