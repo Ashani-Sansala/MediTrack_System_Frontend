5dbddf7e8587dcc5b716021f3aa6527e9d94b45f
Abeypala P.S.D. <abeypalapsd.21@itfac.mrt.ac.lk>
2024-04-28 13:29:10 +0530

    create manage camera page.

---
+++ src/pages/manageCamera/ManageCamera.jsx
@@ -0,0 +1,25 @@
+import SmallButton from '../../components/Main/SmallButton.jsx';
+import '../manageCamera/ManageCamera.scss'
+const ManageCamera = () => {
+
+    return (
+        <div>
+            <h1>Manage Camera</h1>
+            <div className="table-container">
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Camera Id</th>
+                        <th>Location</th>
+                        <th>Remove camera</th>
+                    </tr>
+                    </thead>
+                </table>
+                <SmallButton className="small_button_green"  onClick={onclick}/>
+            </div>
+
+        </div>
+    );
+};
+
+export default ManageCamera;
