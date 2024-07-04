76f0af70bc26b5d160ef945d0527b7daf635b1ad
Nishanth <nishanthp.21@uom.lk>
2024-07-04 22:25:53 +0530

    some pages changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.scss';
+import { useEffect } from 'react';
+import SecureLS from 'secure-ls';
 import Dashboard from './pages/dashboard/Dashboard.jsx';
 import MainLayout from './layouts/MainLayout.jsx';
 import VideoFeed from './pages/videoFeed/VideoFeed.jsx';
@@ -7,23 +8,47 @@ import ManageUsers from './pages/manageUsers/ManageUsers.jsx';
 import Layout01 from './layouts/Layout01.jsx';
 import ManageCamera from './pages/manageCamera/ManageCamera.jsx';
 import { Login } from './pages/login/client/loginPage/Login.jsx';
-import RegistrationForm from './pages/registrationForm/RegistrationForm.jsx';
+import useLogout from './components/Logout/Logout.jsx'; 
+import HistoricalRecords from './pages/historicalRecords/HistoricalRecords.jsx';
+import './App.scss';
+
+const admin_userid = import.meta.env.VITE_ADMIN_USERID;
+
+const ls = new SecureLS({ encodingType: 'aes' });
+const pID = ls.get('pID'); 
+
+function ProtectedRoute({ element, allowedPIDs }) {
+    const logout = useLogout();
+
+    useEffect(() => {
+        if (!allowedPIDs.includes(pID)) {
+            logout();
+        }
+    }, [allowedPIDs, logout, pID]);
+
+    return element;
+}
 
 export default function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Layout01 />}>
-                    {/* Nested Routes under "/" */}
-                    <Route path="/" element={<Login />} />
+                    <Route index element={<Login />} />
                 </Route>
-
-                <Route path="/signin" element={<MainLayout />}>
+                <Route path="/" element={<MainLayout />}>
                     <Route path="Dashboard" element={<Dashboard />} />
-                    <Route path="ManageCamera" element={<ManageCamera />} />
+                    <Route path="HistoricalRecords" element={<HistoricalRecords />} />
+                    <Route path="ManageCamera" element={
+                        <ProtectedRoute element={<ManageCamera />} allowedPIDs={[admin_userid]} />
+                    } />
                     <Route path="VideoFeed" element={<VideoFeed />} />
-                    <Route path="ManageUsers" element={<ManageUsers />} />
-                    <Route path="RegistrationForm" element={<RegistrationForm />} />
+
+                    <Route path="ManageUsers" element={
+                        <ProtectedRoute element={<ManageUsers />} allowedPIDs={[admin_userid]} />
+                    } />
+                    <Route path="VideoFeed" element={<VideoFeed/>}/>
+
                 </Route>
             </Routes>
         </Router>
