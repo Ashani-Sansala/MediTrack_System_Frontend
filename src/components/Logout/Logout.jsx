e34fb7f2d8ac3131fa97c6c5e719ec3a3c5ea65f
Ashani-Sansala <ashani.sansala16@gmail.com>
2024-07-09 17:48:32 +0530

    Improved Dashboard
    - Change access footage to access frame
    - Add direction of the equipment being moved to the Dashboard
    - Usability improved

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,19 +1,23 @@
 import SecureLS from 'secure-ls';
 import { useNavigate } from 'react-router-dom';
 
+// Initializing SecureLS with AES encryption
 const ls = new SecureLS({ encodingType: 'aes' });
 
+// Custom hook to handle user logout
 const useLogout = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // Initialize the navigate function to handle navigation
 
   const logout = () => {
+    // Remove user-related data from local storage
     ls.remove('fullName');
     ls.remove('username'); 
-    ls.remove('pID');
+    ls.remove('category');
+    ls.remove('avatarUrl');
     navigate('/'); // Redirect to the login page
   };
 
-  return logout;
+  return logout; // Return the logout function so it can be used in components
 };
 
 export default useLogout;
